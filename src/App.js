7b836cd5f5929927c04fbbb8dd3125a606a1b3af
jacksolaro <jacksolaro@gmail.com>
2020-11-11 09:30:48 -0800

    added footer component  and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="">
       <header className="">
         <Nav></Nav>
+        <Footer></Footer>
       </header>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    // <!-- Footer -->
+    <footer className="navbar-dark bg-dark">
+      <div className="container-fluid footer-accent"></div>
+      <div className="container-fluid">
+        <div className="row text-center">
+          <div className="col-lg-12">Copyright&copy; 2020 Jack Solaro</div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;

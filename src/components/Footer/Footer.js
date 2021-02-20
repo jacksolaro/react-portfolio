80f7ccf169e2ac59184266347acdaeb47b20afe3
jacksolaro <jacksolaro@gmail.com>
2021-02-20 12:22:24 -0800

    added logo and icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,29 @@
 import React from "react";
 import "./Footer.css";
+import EmailIcon from "@material-ui/icons/Email";
+import PhoneIcon from "@material-ui/icons/Phone";
+import DescriptionIcon from "@material-ui/icons/Description";
 
 function Footer() {
   return (
     // <!-- Footer -->
     <footer className="">
       <div className="footer-accent"></div>
-      <div className="">
-        <div className="">
-          <div className="">Copyright&copy; 2020 Jack Solaro</div>
-        </div>
+      {/* <div className="Footer__Contents"> */}
+      <h2 className="Footer__Logo">
+        <strong>JACK SOLARO</strong>
+      </h2>
+
+      <div>
+        <i className="Footer__Icon fab fa-github fa-2x"></i>
+        <i className="Footer__Icon fab fa-linkedin fa-2x"></i>
+        <i class="Footer__Icon fas fa-file-pdf fa-2x"></i>
+        <i class="Footer__Icon fas fa-phone-square fa-2x"></i>
+        <i class="Footer__Icon fas fa-envelope-square fa-2x"></i>
       </div>
+
+      <div className="Footer__Copyright">Copyright&copy; 2020 Jack Solaro</div>
+      {/* </div> */}
     </footer>
   );
 }
